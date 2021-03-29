e9760471005906ca81f861e0945efcb845100f94
pkcheng <pkcheng2000@hotmail.com>
2021-03-29 17:53:34 +0800

    fix firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDMV_Xp1JAasQYwBEqLAlexJZwnorpYrg0",
+  authDomain: "nezuko-8c1e7.firebaseapp.com",
+  projectId: "nezuko-8c1e7",
+  storageBucket: "nezuko-8c1e7.appspot.com",
+  messagingSenderId: "209903574596",
+  appId: "1:209903574596:web:983421ce65e31677b9a611",
+  measurementId: "G-CW3F77RG4H",
 });
 
 export const auth = app.auth();
